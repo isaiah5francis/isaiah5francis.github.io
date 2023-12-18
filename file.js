fe965a63a04c14999abfbbbd30ff5a73bf03092f
Isaiah Francis <78046416+isaiah5francis@users.noreply.github.com>
2023-12-18 00:38:21 -0700

    Added Most Recent Files
    God, I hope this works...

--- file.js
+++ file.js
@@ -1,19 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
+
     //Footer
+
     const footer = document.getElementById('my-footer');
     const currentYear = new Date().getFullYear();
     footer.innerHTML = `Copyright © ${currentYear} Your Name`;
 
     //Alert Button
+
     const alertButton = document.getElementById('btn-alert');
     alertButton.addEventListener('click', function () {
         alert('Hello world!');
     });
 
     //Hover Button
+
     const hoverButton = document.getElementById('btn-hover');
     hoverButton.addEventListener('mouseover', function () {
-        hoverButton.innerText = 'New button label';
+    hoverButton.innerText = 'New button label';
     });
 
     hoverButton.addEventListener('mouseleave', function () {
@@ -21,6 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     //Incrementing Button
+
     let count = 1;
     const incrementButton = document.getElementById('btn-counter');
     const counterText = document.getElementById('txt-counter');
@@ -28,21 +33,22 @@ document.addEventListener("DOMContentLoaded", function () {
         count++;
         counterText.innerHTML = `Number: ${count}`;
 
-        //Check if the number is even or odd and update the class
-        if (count % 2 === 0) {
-            counterText.classList.remove('odd');
-            counterText.classList.add('even');
-        } else {
-            counterText.classList.remove('even');
-            counterText.classList.add('odd');
+    //Check if the number is even or odd and update the class
+    if (count % 2 === 0) {
+        counterText.classList.remove('odd');
+        counterText.classList.add('even');
+    } else {
+        counterText.classList.remove('even');
+        counterText.classList.add('odd');
         }
     });
 
     //Loops
+
     const numbersList = document.getElementById('numbers');
     for (let i = 1; i <= 100; i++) {
-        const listItem = document.createElement('li');
-        listItem.textContent = i % 2 === 0 ? 'even' : 'odd';
-        numbersList.appendChild(listItem);
+    const listItem = document.createElement('li');
+    listItem.textContent = i % 2 === 0 ? 'even' : 'odd';
+    numbersList.appendChild(listItem);
     }
 });
